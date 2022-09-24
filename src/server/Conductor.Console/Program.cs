2f4bd9e13b98e91efb5356f9299712838e06e448
Jaime Still <jpstill85@gmail.com>
2022-09-23 20:34:15 -0400

    initial Sql services established

---
+++ src/server/Conductor.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Nodes;
+using Conductor.Models.Entities;
+using Conductor.Services.Sql;
+// See https://aka.ms/new-console-template for more information
+
+Statement statement = new()
+{
+    Name = "Product Test",
+    Value = "SELECT top (1000)\n\tp.Name as 'name',\n\tp.ProductNumber as 'name',\n\tp.ProductId as 'id'\nFROM [AdventureWorks2019].[Production].[Product] as p\nWHERE p.Name like '%{{search}}%'",
+    Connector = new()
+    {
+        Database = "AdventureWorks2019",
+        Server = @".\DevSql",
+        Name = "DevSql AdventureWorks"
+    }
+};
+
+Console.WriteLine(statement.Interpolate("search:black"));
+Console.WriteLine();
+
+JsonArray result = await SqlConnector.Execute(statement.Connector, statement.Interpolate("search:black"));
+
+foreach (JsonNode node in result)
+    Console.WriteLine(node.ToJsonString());
--- src/server/Conductor.Models/Entities/Statement.cs
+++ src/server/Conductor.Models/Entities/Statement.cs
@@ -4,5 +4,29 @@ public class Statement : Entity
     public int ConnectorId { get; set; }
     public string Value { get; set; }
 
-    public Connector Connector { get; set; }    
+    public Connector Connector { get; set; }
+
+    public string Interpolate(string props)
+    {
+        string result = Value;
+
+        if (props.Contains('/'))
+        {
+            foreach (var prop in props.Split('/'))
+            {
+                if (prop.Contains(':'))
+                    result = Interpolate(result, prop);                    
+            }
+        }
+        else if (props.Contains(':'))
+            result = Interpolate(result, props);
+
+        return result;
+    }
+
+    static string Interpolate(string script, string prop) =>
+        script.Replace(
+            $"{{{{{prop.Split(':')[0]}}}}}",
+            prop.Split(':')[1]
+        );
 }
