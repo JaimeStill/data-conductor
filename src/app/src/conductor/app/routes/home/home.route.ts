4421b3ee8fe6e108c6dbe32acd6ba96237e02fdd
Jaime Still <jpstill85@gmail.com>
2022-09-27 22:27:43 -0400

    some initial layout stuff

--- src/app/src/conductor/app/routes/home/home.route.ts
+++ src/app/src/conductor/app/routes/home/home.route.ts
@@ -2,15 +2,25 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { QuerySource, Statement } from '../../models';
+
+import { StatementApi } from '../../services';
 
 @Component({
   selector: 'home-route',
   templateUrl: 'home.route.html'
 })
 export class HomeRoute implements OnInit {
+  statementSrc: QuerySource<Statement>;
+
   constructor(
+    public statementApi: StatementApi
   ) { }
 
   ngOnInit() {
+    this.statementSrc = this.statementApi.query();
   }
+
+  download = (statement: Statement) =>
+    this.statementApi.download(statement);
 }
